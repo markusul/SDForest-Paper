9497c50fa3ac7b734e88c2c8c8b1eade9db75f7f
Markus Ulmer <markus.ulmer@stat.math.ethz.ch>
2024-09-02 12:14:49 +0200

    comparison fast/b

---
+++ simulation_study/fast.R
@@ -0,0 +1,32 @@
+library(SDForest)
+library(parallel)
+library(tidyr)
+
+mc.cores <- 100
+
+performance_measure <- function(n, p, q, n_test){
+  data <- simulate_data_step(q = q, p = p, n = n+n_test, m = 10)
+  data_train <- data.frame(data$X[1:n,], Y = data$Y[1:n])
+  data_test <- data.frame(data$X[(n+1):(n+n_test),], Y = data$Y[(n+1):(n+n_test)])
+  
+  fit <- SDTree(Y ~ ., data_train)
+  pred <- predict(fit, data_test)
+  
+  fit <- SDTree(Y ~ ., data_train, fast = FALSE)
+  pred2 <- predict(fit, data_test)
+  
+  mse <- (data$f_X[(n+1):(n+n_test)] - pred)
+  mse2 <- (data$f_X[(n+1):(n+n_test)] - pred2)
+  
+  return(list(SDT_fast = mse, SDT2= mse2))
+}
+
+
+perf <- mclapply(1:100, function(i) sapply(performance_measure(1000, 500, 20, 500), 
+                                           function(x)mean(x**2)), mc.cores = mc.cores)
+perf <- do.call(rbind, perf)
+perf_g <- gather(data.frame(perf), key = 'method', value = 'performance')
+
+perf_g
+save(perf_g,
+     file = "simulation_study/results/fast.RData")

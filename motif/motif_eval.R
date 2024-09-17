1b0a2ecf4745c99fab3a4c17cac8d341e35a573f
Markus Ulmer <markus.ulmer@stat.math.ethz.ch>
2024-09-17 17:09:30 +0200

    update plots

--- motif/motif_eval.R
+++ motif/motif_eval.R
@@ -1,1 +1,32 @@
 library(SDForest)
+
+load('motif/results/fits.RData')
+
+fit_dec
+fit_plain
+
+imp_dec <- fit_dec$var_importance
+imp_dec <- imp_dec / max(imp_dec)
+
+imp_plain <- fit_plain$var_importance
+imp_plain <- imp_plain / max(imp_plain)
+
+plot(imp_plain, imp_dec)
+
+plot(sort(imp_dec))
+points(sort(imp_plain), col = 3)
+
+
+path_dec <- regPath(fit_dec)
+path_plain <- regPath(fit_plain)
+
+stpath_dec <- stabilitySelection(fit_dec)
+stpath_plain <- stabilitySelection(fit_plain)
+
+
+plot(path_dec)
+plot(path_plain)
+
+plot(stpath_dec)
+plot(stpath_plain)
+

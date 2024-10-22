d5fccef175e82ecb806673e8d0a9202629fe84ae
Markus Ulmer <markus.ulmer@stat.math.ethz.ch>
2024-10-22 15:31:07 +0200

    eval of motif

--- motif/motif_eval.R
+++ motif/motif_eval.R
@@ -38,6 +38,8 @@ fit_plain_pruned <- prune(fit_plain, path_plain$cp_min)
 
 imp_dec_pruned <- fit_dec_pruned$var_importance
 imp_dec_pruned <- imp_dec_pruned / max(imp_dec_pruned)
+names(imp_dec_pruned) <- 1:length(imp_dec_pruned)
+
 
 imp_plain_pruned <- fit_plain_pruned$var_importance
 imp_plain_pruned <- imp_plain_pruned / max(imp_plain_pruned)
@@ -64,3 +66,42 @@ plot(dep_plain278)
 idx <- which(imp_dec_pruned %in% sort(imp_dec_pruned, decreasing = T)[1:20])
 hist(apply(motifs, 2, var), breaks = 100)
 idx
+
+
+
+
+library(SDForest)
+library(ggplot2)
+
+load('motif/results/2fits131.RData')
+
+path_dec <- regPath(fit_dec)
+plotOOB(path_dec)
+
+fit_dec <- prune(fit_dec, path_dec$cp_min)
+
+path_plain <- regPath(fit_plain)
+plotOOB(path_plain)
+
+fit_plain <- prune(fit_plain, path_plain$cp_min)
+
+imp_dec <- fit_plain$var_importance
+names(imp_dec) <- 1:length(imp_dec)
+
+
+plot(sort(fit_dec$var_importance, decreasing = T))
+plot(sort(fit_plain$var_importance, decreasing = T))
+
+sort(imp_dec, decreasing = T)[1:20]
+
+
+
+#41        635         30        595         29        602        343        136         37 
+#0.22495592 0.19590564 0.14524306 0.11424881 0.11000748 0.09047771 0.04226199 0.02669918 0.02541486 
+#198        634        622        347        210        605        630        650        619 
+#0.02539691 0.02132598 0.02001209 0.01725338 0.01547816 0.01434881 0.01385085 0.01339443 0.01157428 
+#613         99 
+#0.01150529 0.01102374
+
+
+sort(imp_dec_pruned, decreasing = T)[1:20]

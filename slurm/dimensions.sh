5fa67e5903089eab3e13b98c213625242f53deb9
markus <markus_ulmer@bluewin.ch>
2025-05-21 15:36:35 +0200

    remove unnecessary stuf

--- slurm/dimensions.sh
+++ slurm/dimensions.sh
@@ -2,11 +2,10 @@ module load stack/2024-06  gcc/12.2.0
 module load r/4.4.0
 export OMP_NUM_THREADS=1
 
-for i in {12..12}
+for i in {1..12}
 do 
    sbatch --time=128:00:00 --job-name="n $i" --mem-per-cpu=2GB --output=outfiles/n_$i.out --cpus-per-task=100 --wrap "Rscript --vanilla simulation_study/perf_n.r $i"
-   #sbatch --time=48:00:00 --job-name="p $i" --mem-per-cpu=2GB --output=outfiles/p_$i.out --cpus-per-task=100 --wrap "Rscript --vanilla simulation_study/perf_p.r $i"
-   #sbatch --time=128:00:00 --job-name="q $i" --mem-per-cpu=2GB --output=outfiles/q_$i.out --cpus-per-task=100 --wrap "Rscript --vanilla simulation_study/perf_q.r $i"
-   #sbatch --time=128:00:00 --job-name="eff $i" --mem-per-cpu=2GB --output=outfiles/eff_$i.out --cpus-per-task=100 --wrap "Rscript --vanilla simulation_study/dense_assumption.r $i"
-   #sbatch --time=128:00:00 --job-name="lim1 $i" --mem-per-cpu=2GB --output=outfiles/lim1_$i.out --cpus-per-task=100 --wrap "Rscript --vanilla simulation_study/limitations_1.r $i"
+   sbatch --time=48:00:00 --job-name="p $i" --mem-per-cpu=2GB --output=outfiles/p_$i.out --cpus-per-task=100 --wrap "Rscript --vanilla simulation_study/perf_p.r $i"
+   sbatch --time=128:00:00 --job-name="q $i" --mem-per-cpu=2GB --output=outfiles/q_$i.out --cpus-per-task=100 --wrap "Rscript --vanilla simulation_study/perf_q.r $i"
+   sbatch --time=128:00:00 --job-name="eff $i" --mem-per-cpu=2GB --output=outfiles/eff_$i.out --cpus-per-task=100 --wrap "Rscript --vanilla simulation_study/dense_assumption.r $i"
 done
